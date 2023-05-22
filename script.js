cee54ed604de44df65221842be43a6828be8b357
ajaythoram <115677014+ajaythoram@users.noreply.github.com>
2023-05-22 07:48:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 const byteSize = (str) => {
   // write your code here
+	const encoder = new TextEncoder();
+  const encodedData = encoder.encode(str);
+	
+  const byteCount = encodedData.length;
+  return byteCount;
+	
 };
 
 // Do not change the code below
